add469c40627dabea7cab212fd5aee5ecbc83ff4
Miles Andrioli <miles@gmail.com>
2023-02-26 03:09:15 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './css/App.min.css'
+import Carousel from './Carousel'
+
+function App() {
+  return (
+    <>
+      <Carousel background="#444" alt="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil aliquam, obcaecati corporis reiciendis similique dolore sint repudiandae quia ullam quod. Nesciunt, sit voluptatem ipsum nihil quos libero necessitatibus sed magnam." />
+
+      <Carousel background="#555" alt="Consectetur adipisicing elit. Nihil aliquam, obcaecati corporis reiciendis similique dolore necessitatibus sed magnam." />
+
+      <Carousel background="#666" alt="Repudiandae quia ullam quod. Nesciunt, sit voluptatem ipsum nihil quos libero necessitatibus sed magnam." />
+    </>
+  )
+}
+
+export default App
---
+++ src/Carousel.js
@@ -0,0 +1,4 @@
+function Carousel(props) {
+    return <h1 style={{ backgroundColor: props.background }}>{props.alt}</h1>
+}
+export default Carousel
